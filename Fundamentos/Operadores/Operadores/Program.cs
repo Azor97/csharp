de6e5f8d1106a238b618df9f6884b25150097a54
Daniel <daniel.pessoa2010@hotmail.com>
2023-02-28 00:11:45 -0300

    Initial commit

---
+++ Fundamentos/Operadores/Operadores/Program.cs
@@ -0,0 +1,35 @@
+﻿//Operadores aritmeticos
+int adicao = 5 + 3;
+int subtracao = 5 - 3;
+int multiplicacao = 5 * 3;
+int divisao = 15 / 3;
+
+int expr = (1 + 2) * 5 - 2 / 2;
+
+//Operadores relacionais
+string cidade1 = "Belo Horizonte";
+string cidade2 = "Rio de Janeiro";
+
+bool ret1 = cidade1 == cidade2;
+bool ret2 = cidade2 != cidade1;
+
+int i = 2;
+int j = 5;
+
+bool ret3 = i == j;
+bool ret4 = i != j;
+bool ret5 = i > j;
+bool ret6 = i < j;
+bool ret7 = i >= j;
+bool ret8 = i <= j;
+
+//Operadores logicos
+bool ret9 = i != j && ret1 == ret2; //false
+bool ret10 = i < j && ret1 != ret2; //true
+
+bool ret11 = i > j || ret1 != ret2; //true
+
+bool ret12 = !(i < j);
+
+
+Console.WriteLine($"{ret12}");
---
+++ Fundamentos/TiposDeDados.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Class1
+{
+	public class Tipos()
+	{
+		public void ConhecendoOsTipos()
+	{
+		//int
+		int i = 10;
+
+		//float
+		float f = 10.9f; // Precisao baixa, tamanho 4 bytes. Exemplos: Trabalhando com numeros cientificos.
+
+        //double
+        double d = 20.9d; // Precisao baixa, tamanho 8 bytes. Exemplos: Trabalhando com numeros cientificos.
+
+		//decimal
+		decimal c = 21.8; // Precisao exata, tamanho 16 bytes. Exemplos: Trabalhando com dinheiro, valores de produtos.
+
+		//bool
+		bool b1 = true;
+		bool b2 = false;
+
+		//chat
+		char c2 = 'A';
+
+		//string
+		string s2 = "Azor Ahai";
+
+		//Atribuicao implicita:
+		var cidade = "Belo Horizonte"; //String
+		var idade = 30; //int
+
+		idade = 55;
+
+		//Constantes nao podem ser alteradas.
+		const string pais = "Brasil";
+
+		//Conversao de tipos
+		f = idade;
+		f = (float)c; //Conversao explicita
+
+	}
+	}
+}
