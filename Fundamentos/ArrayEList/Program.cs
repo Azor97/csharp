41cb4556a47230d84d87a7fbc773c5ded620fcdf
Daniel <daniel.pessoa2010@hotmail.com>
2023-02-28 20:39:12 -0300

    Novas Aulas

---
+++ Fundamentos/ArrayEList/Program.cs
@@ -0,0 +1,27 @@
+﻿// Array se usa quando ja temos uma quantidade definida de elementos que vao compor esses agrupamento.
+// Colecoes sao usadas quando nao tem quantidade definida.
+
+int[] numeros = new int[] { 1, 2, 3, 4, 5 };
+int num = numeros[0];
+// Console.WriteLine(num);
+
+int total = numeros.Length;
+// Console.WriteLine(total);
+
+List<int> listNumeros = new List<int>();
+List<int> listaNums = new List<int>() { 1, 2, 3, 4, 5 };
+
+int count = listaNums.Count;
+listaNums.Add(6);
+listaNums.Remove(3);
+
+int numer = listaNums.ElementAt(0);
+Console.WriteLine(numer);
+
+listaNums.Clear();
+// Console.WriteLine(count);
+
+// foreach (int numero in listaNums)
+//     Console.WriteLine(numero);
+
+
---
+++ Fundamentos/Dictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿Dictionary<int, string> estados = new Dictionary<int, string>();
+estados.Add(1, "MG");
+estados.Add(2, "SP");
+estados.Add(3, "PR");
+
+if (!estados.ContainsKey(4))
+{
+    estados.Add(4, "SC");
+}
+
+foreach (string estado in estados.Values)
+{
+    // Console.WriteLine(estado);
+}
+
+string est = null; // Variavel nao tem valor
+
+if (estados.TryGetValue(2, out est))
+{
+    Console.WriteLine(est);
+}
+
